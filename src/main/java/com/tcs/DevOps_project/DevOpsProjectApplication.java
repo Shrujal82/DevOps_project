cefd17e3314883bec3b2b81478cac43af15b1df3
Shrujal82 <parmar8297shrujal@gmail.com>
2025-11-21 16:59:57 +0530

    initial commit

---
+++ src/main/java/com/tcs/DevOps_project/DevOpsProjectApplication.java
@@ -0,0 +1,20 @@
+package com.tcs.DevOps_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DevOpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjectApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcom to demo project</h1>";
+	}
+}
